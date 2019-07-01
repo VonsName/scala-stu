063adcf4973f2e0698d71f31b50c5579203ad217
fjl <fengjianglin6817@dingtalk.com>
2019-07-01 15:57:39 +0800

    finish

--- scala-akka/src/main/scala/com/von/akka/actor/ActorDemo02.scala
+++ scala-akka/src/main/scala/com/von/akka/actor/ActorDemo02.scala
@@ -5,7 +5,7 @@ import akka.actor.{Actor, ActorSelection}
 /**
   * @author ： fjl
   * @date ： 2019/6/28/028 16:15
-  *      247
+  *       247
   */
 class ActorDemo02(serverHost: String, serverPort: Int) extends Actor {
 
@@ -15,9 +15,13 @@ class ActorDemo02(serverHost: String, serverPort: Int) extends Actor {
     //Server是服务端创建ActorSystem的时候指定的名字
     //a1 是对应服务端Actor的名字
     actorRef = context.actorSelection(s"akka.tcp://Server@$serverHost:$serverPort/user/a1")
+    //定时器
+//    context.system.scheduler.schedule(Duration.Zero, Duration.create(3000,TimeUnit.MICROSECONDS), self, HeartBeat)
   }
 
   override def receive: Receive = {
     case _ => println("no")
   }
 }
+
+case object HeartBeat
---
+++ src/main/scala/com/chapter11/GenericsTestDemo.scala
@@ -0,0 +1,11 @@
+package com.chapter11
+
+/**
+  * @author ： fjl
+  * @date ： 2019/7/1/001 15:41
+  */
+object GenericsTestDemo {
+
+}
+
+
