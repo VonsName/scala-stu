7e7f3c3830df24e7f7ae0b4e128581befeb6bce4
894611653@qq.com <894611653@qq.com>
2019-09-15 11:10:23 +0800

    rocket

--- src/main/java/RocketmqConsumer.java
+++ src/main/java/RocketmqConsumer.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
 
 /**
  * @author : vons
@@ -6,4 +13,23 @@ package PACKAGE_NAME;
  * @date : 2019/8/23 20:21
  */
 public class RocketmqConsumer {
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer pushConsumer = new DefaultMQPushConsumer();
+        pushConsumer.setNamesrvAddr("117.78.5.14:9876");
+        pushConsumer.subscribe("OrderTopic", "OrderTag");
+        pushConsumer.setConsumerGroup("order-rocket");
+        pushConsumer.setVipChannelEnabled(false);
+        pushConsumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+
+                for (MessageExt messageExt : list) {
+                    System.out.println("消费端接收消息：" + new String(messageExt.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        pushConsumer.start();
+    }
 }
--- src/main/java/RocketmqProducer.java
+++ src/main/java/RocketmqProducer.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 
 /**
  * @author : vons
@@ -6,4 +10,20 @@ package PACKAGE_NAME;
  * @date : 2019/8/23 20:20
  */
 public class RocketmqProducer {
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        DefaultMQProducer defaultMQProducer = new DefaultMQProducer();
+        defaultMQProducer.setNamesrvAddr("117.78.5.14:9876");
+        defaultMQProducer.setProducerGroup("order-rocket");
+        defaultMQProducer.setVipChannelEnabled(false);
+        defaultMQProducer.start();
+        for (int i = 0; i < 50; i++) {
+            Message message = new Message();
+            message.setTopic("OrderTopic");
+            message.setTags("OrderTag");
+            message.setBody(("HELLO" + i).getBytes());
+            defaultMQProducer.send(message);
+            System.out.println("发送消息");
+            Thread.sleep(1000);
+        }
+    }
 }
